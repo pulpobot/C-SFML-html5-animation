20db5c5b732b3e5f8422bc0093644dcad31f3f6e
Santiago Alvarez <saalvarezva@gmail.com>
2017-02-05 01:29:03 -0500

    Added - Examples 02 and 03 of Chapter 03

---
+++ ch03-03-bobbing-2/Source/ch03-03-bobbing-2/main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics.hpp>
+#include "Ball.h"
+#include <math.h>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Bobbing 2", sf::Style::Titlebar | sf::Style::Close);
+	window.setFramerateLimit(60);
+
+	float angle = 0;
+	float speed = 0.05f;
+	float range = 50;
+	float centerY = window.getSize().y / 2.0f;
+
+	Ball * ball = new Ball(window.getSize().x / 2.0f, centerY, 40, sf::Color::Red);
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+						break;
+			}
+		}
+
+		angle += speed;
+		ball->shape.setPosition(ball->shape.getPosition().x, centerY + std::sinf(angle) * range);
+
+		window.clear(sf::Color::White);
+		window.draw(ball->shape);
+		window.display();
+	}
+
+
+}
