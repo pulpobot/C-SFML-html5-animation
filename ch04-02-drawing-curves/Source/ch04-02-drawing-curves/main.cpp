a9ab43ffe845a7d1a89bf38ecf460e15b320667b
Santiago Alvarez <saalvarezva@gmail.com>
2017-02-05 19:10:32 -0500

    Examples 01 and 02 of Chapter 04
    Example ch04-02-drawing-curves has an util function to calculate
    quadratic beziers curves

---
+++ ch04-02-drawing-curves/Source/ch04-02-drawing-curves/main.cpp
@@ -0,0 +1,42 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Utils.h"
+
+int main()
+{
+	//You can turn off antialiasing if your graphics card doesn't support it
+	sf::ContextSettings context;
+	context.antialiasingLevel = 4;
+
+	sf::RenderWindow window(sf::VideoMode(400,400), "Drawing App", sf::Style::Titlebar | sf::Style::Close, context);
+	window.setFramerateLimit(60);
+
+	std::vector<sf::Vertex> bezierCurvePoints;
+	Utils::QuadraticBezierCurve(sf::Vector2f(100, 200), sf::Vector2f(200,200), sf::Vector2f(300, 200), 20, bezierCurvePoints, sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::MouseMoved:
+					//While the mouse button is being pressed, add a new vertex to the current line with the mouse position
+					Utils::QuadraticBezierCurve(sf::Vector2f(100, 200), sf::Vector2f(event.mouseMove.x, event.mouseMove.y), sf::Vector2f(300, 200), 20, bezierCurvePoints, sf::Color::Red);
+					break;
+			}
+		}
+
+		window.clear(sf::Color::White);
+
+		window.draw(&bezierCurvePoints[0], bezierCurvePoints.size(), sf::LinesStrip);
+
+		window.display();
+	}
+
+	return 0;
+}
