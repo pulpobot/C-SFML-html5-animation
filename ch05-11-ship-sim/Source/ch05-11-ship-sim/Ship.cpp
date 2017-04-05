7aab095e109a8d48873768134e908a73eed365cd
Santiago Alvarez <saalvarezva@gmail.com>
2017-04-04 21:25:25 -0500

    Chapter 05: Examples 09-11
    Chapter 05 Completed

---
+++ ch05-11-ship-sim/Source/ch05-11-ship-sim/Ship.cpp
@@ -0,0 +1,88 @@
+#include "Ship.h"
+
+Ship::Ship(float x, float y)
+{
+	shape.setPointCount(4);
+	shape.setPoint(0, sf::Vector2f(10, 0));
+	shape.setPoint(1, sf::Vector2f(-10, 10));
+	shape.setPoint(2, sf::Vector2f(-5, 0));
+	shape.setPoint(3, sf::Vector2f(-10, -10));
+
+	shape.setPosition(x, y);
+	shape.setFillColor(sf::Color::Transparent);
+	shape.setOutlineThickness(1);
+	shape.setOutlineColor(sf::Color::White);
+
+	flame.setPointCount(4);
+	flame.setPoint(0, sf::Vector2f(-5.1f, 0));
+	flame.setPoint(1, sf::Vector2f(-8.0f, -5));
+	flame.setPoint(2, sf::Vector2f(-15, 0));
+	flame.setPoint(3, sf::Vector2f(-8.0f, 5));
+
+	flame.setPosition(x, y);
+	flame.setFillColor(sf::Color::Transparent);
+	flame.setOutlineThickness(1);
+	flame.setOutlineColor(sf::Color::White);
+}
+
+Ship::~Ship()
+{
+}
+
+void Ship::Draw(sf::RenderWindow * render)
+{
+	render->draw(shape);
+	if (showFlame)
+		render->draw(flame);
+}
+
+void Ship::SetPosition(float x, float y)
+{
+	shape.setPosition(sf::Vector2f(x, y));
+	flame.setPosition(sf::Vector2f(x, y));
+}
+
+void Ship::SetX(float x)
+{
+	shape.setPosition(sf::Vector2f(x, shape.getPosition().y));
+	flame.setPosition(sf::Vector2f(x, flame.getPosition().y));
+}
+
+void Ship::SetY(float y)
+{
+	shape.setPosition(sf::Vector2f(shape.getPosition().x, y));
+	flame.setPosition(sf::Vector2f(flame.getPosition().x, y));
+}
+
+float Ship::GetX()
+{
+	return shape.getPosition().x;
+}
+
+float Ship::GetY()
+{
+	return shape.getPosition().y;
+}
+
+void Ship::Translate(float x, float y)
+{
+	shape.setPosition(sf::Vector2f(shape.getPosition().x + x, shape.getPosition().y + y));
+	flame.setPosition(sf::Vector2f(flame.getPosition().x + x, flame.getPosition().y + y));
+}
+
+void Ship::SetRotation(float angle)
+{
+	shape.setRotation(angle);
+	flame.setRotation(angle);
+}
+
+float Ship::GetRotation()
+{
+	return shape.getRotation();
+}
+
+void Ship::Rotate(float angle)
+{
+	shape.setRotation(shape.getRotation() + angle);
+	flame.setRotation(flame.getRotation() + angle);
+}
---
+++ ch05-11-ship-sim/Source/ch05-11-ship-sim/Ship.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "SFML\Graphics.hpp"
+
+class Ship
+{
+public:
+	Ship(float x, float y);
+	~Ship();
+	void Draw(sf::RenderWindow * render);
+
+	sf::ConvexShape shape;
+	bool showFlame = false;
+	//Overrides current position
+	void SetPosition(float x, float y);
+	void SetX(float x);
+	void SetY(float y);
+	float GetX();
+	float GetY();
+	//Increase current position by x and y values
+	void Translate(float x, float y);
+
+	//Overrides current rotation (in degrees)
+	float GetRotation();
+	//Overrides current rotation (in degrees)
+	void SetRotation(float angle);
+	//Increase current rotation (in degrees)
+	void Rotate(float angle);
+
+private:
+	sf::ConvexShape flame;
+};
+
