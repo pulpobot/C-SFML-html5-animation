74002342b65c5998905a4ea2ba8465026e97f6d7
Santiago Alvarez <saalvarezva@gmail.com>
2017-10-20 18:22:38 -0500

    Chapter 10 - Example 04

---
+++ ch10-04-angle-bounce/src/Line.cpp
@@ -0,0 +1,61 @@
+#include "Line.h"
+#include <math.h>
+#include <iostream>
+#include "SFML\Graphics.hpp"
+
+Line::Line(float x, float y, float length, sf::Color color) {
+    shape = sf::RectangleShape(sf::Vector2f(length, 2));
+    shape.setFillColor(color);
+    shape.setPosition(x, y);
+    vx = 0;
+    vy = 0;
+}
+
+Line::~Line() {
+}
+
+void Line::SetPosition(float x, float y) {
+    shape.setPosition(sf::Vector2f(x, y));
+}
+
+void Line::SetX(float x) {
+    shape.setPosition(sf::Vector2f(x, shape.getPosition().y));
+}
+
+void Line::SetY(float y) {
+    shape.setPosition(sf::Vector2f(shape.getPosition().x, y));
+}
+
+float Line::GetX() {
+    return shape.getPosition().x;
+}
+
+float Line::GetY() {
+    return shape.getPosition().y;
+}
+
+void Line::Translate(float x, float y) {
+    shape.setPosition(sf::Vector2f(shape.getPosition().x + x, shape.getPosition().y + y));
+}
+
+void Line::SetRotation(float angle) {
+    shape.setRotation(angle);
+}
+
+void Line::Rotate(float angle) {
+    shape.setRotation(angle + shape.getRotation());
+}
+
+float Line::Rotation() {
+    shape.getRotation();
+}
+
+void Line::Draw(sf::RenderWindow &renderWindow, float gravity) {
+    vy += gravity;
+    Translate(vx, vy);
+    renderWindow.draw(shape);
+}
+
+sf::FloatRect Line::GetBounds() {
+    return shape.getGlobalBounds();
+}
---
+++ ch10-04-angle-bounce/src/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include "SFML\Graphics.hpp"
+#include "Line.h"
+#include "Ball.h"
+
+const float GRAVITY = 0.2f;
+const float BOUNCE = -0.6f;
+
+int main() {
+    //You can turn off antialiasing if your graphics card doesn't support it
+    sf::ContextSettings context;
+    context.antialiasingLevel = 4;
+
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Angle Bounce", sf::Style::Titlebar | sf::Style::Close,
+                            context);
+    window.setFramerateLimit(60);
+
+    Ball ball(100, 100, 40, sf::Color::Red);
+
+    Line line(50, 200, 300, sf::Color::Black);
+    line.Rotate(10);
+
+    float cos = std::cos(line.Rotation() * M_PI / 180);
+    float sin = std::sin(line.Rotation()* M_PI / 180);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
+
+        //normal motion code
+        ball.vy += GRAVITY;
+        ball.Translate(ball.vx, ball.vy);
+
+        //get position of ball, relative to line
+        float x1 = ball.GetX() - line.GetX();
+        float y1 = ball.GetY() - line.GetY();
+
+        //rotate coordinates
+        float x2 = cos * x1 + sin * y1;
+        float y2 = cos * y1 - sin * x1;
+
+        //rotate velocity
+        float vx1 = cos * ball.vx + sin * ball.vy;
+        float vy1 = cos * ball.vy - sin * ball.vx;
+
+        //perform bounce with rotated values
+        if(y2 > -ball.shape.getRadius()){
+            y2 = -ball.shape.getRadius();
+            vy1 *= BOUNCE;
+        }
+
+        //rotate everything back
+        x1 = cos * x2 - sin * y2;
+        y1 = cos * y2 + sin * x2;
+        ball.vx = cos * vx1 - sin * vy1;
+        ball.vy = cos * vy1 + sin * vx1;
+        ball.SetX(line.GetX() + x1);
+        ball.SetY(line.GetY() + y1);
+
+        window.clear(sf::Color::White);
+        window.draw(ball.shape);
+        window.draw(line.shape);
+        window.display();
+    }
+}
